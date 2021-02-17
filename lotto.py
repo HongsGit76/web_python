1cf7460eeadff25cb48e5997338e4ca87b382f4e
KIM TAE HONG <66664373+HongsGit76@users.noreply.github.com>
2021-02-17 23:27:53 +0900

    Delete lotto.py

--- lotto.py
+++
@@ -1,8 +0,0 @@
-import random
-
-lotto_num = []
-while len(lotto_num) < 7:
-    lotto_number = random.randint(1,46)
-    if not lotto_number in lotto_num:
-        lotto_num.append(lotto_number)
-print(lotto_num)
